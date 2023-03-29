b1f543570c8fa0926104b0e7d5457ea2028330c3
Jan Dolanský <dolanovsky@gmail.com>
2023-03-30 01:54:10 +0300

    Add application header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang='ts'>
-
+import Navigation from './components/navigation/Navigation.vue'
 </script>
 
 <template>
   <div>
+    <Navigation />
     <RouterView />
   </div>
 </template>
---
+++ frontend/src/components/navigation/Navigation.vue
@@ -0,0 +1,34 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <header>
+    <RouterLink :to="{ name: 'RouteHome' }" class="disable-hover">
+      <img src="/icons/logo.svg" alt="">
+    </RouterLink>
+
+    <div class="flex-1" />
+
+    <nav>
+      <ul>
+        <li>
+          <RouterLink :to="{ name: 'RouteHome' }">
+            Aliases
+          </RouterLink>
+        </li>
+        <li><a href="/">Create</a></li>
+        <li><a href="/">Your Projects</a></li>
+      </ul>
+    </nav>
+
+    <div class="flex-1" />
+
+    <a href="" class="user">
+      <div class="pfp">
+        <strong>D</strong>
+      </div>
+      <span>dolanske</span>
+    </a>
+  </header>
+</template>
